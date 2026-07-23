2454f2ec2120e48977649c2c9f7b1a1e916257c9
joon0447 <joon0447@naver.com>
2026-07-23 20:53:56 +0900

    build: 앱 버전 업데이트 (0.0.2)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "com.joon.ringout"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2026001
-        versionName = "0.0.1"
+        versionCode = 2026002
+        versionName = "0.0.2"
         buildConfigField("String", "KAKAO_NATIVE_APP_KEY", "\"$escapedKakaoNativeAppKey\"")
         manifestPlaceholders["kakaoRestApiKey"] = kakaoRestApiKey
     }
