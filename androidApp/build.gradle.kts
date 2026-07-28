18c238abe056006a9203e7e220af96a2dc9d504c
joon0447 <joon0447@naver.com>
2026-07-28 19:56:09 +0900

    chore: 안드로이드 앱 버전 업데이트 (0.0.5)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "com.joon.ringout"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2026004
-        versionName = "0.0.4"
+        versionCode = 2026005
+        versionName = "0.0.5"
         buildConfigField("String", "KAKAO_NATIVE_APP_KEY", "\"$escapedKakaoNativeAppKey\"")
         manifestPlaceholders["kakaoRestApiKey"] = kakaoRestApiKey
     }
